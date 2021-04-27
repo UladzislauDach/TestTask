337896d9fc1c8bb4a575480ed538135afb3e0318
UladzislauDach <vladzayac1@gmail.com>
2021-04-27 23:07:48 +0300

    fix any bugs

--- src/main/java/TestAssignment.java
+++ src/main/java/TestAssignment.java
@@ -12,6 +12,7 @@ public class TestAssignment {
         for (int i = 0; i < result.length; i++) {
             for (int j = 0; j < result[0].length; j++) {
                 System.out.print("" + result[i][j] + " ");
+
             }
             System.out.println();
         }
